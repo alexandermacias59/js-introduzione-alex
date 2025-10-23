55e485ddff12bf86290a63dd51b6ae3746f3d703
Alexander Macias <alexandermacias59@gmail.com>
2025-10-23 18:54:32 +0200

    esercizi per casa

--- esercizi.js
+++ esercizi.js
@@ -45,11 +45,8 @@
 
 
 //esercizio 6, fai un ciclo for che stampi il quadrato dei numeri da 1 a 100
-
-
 // for (let i = 0; i <= 100; i++) {
 //     console.log(Math.pow(i,2))
-    
 // }
 
 //esercizio 7, fai un ciclo che per i numer ida uno a 100 stampi pari se il numero è pari e dispari se il numero è dispari
@@ -66,5 +63,40 @@
 
 
 //Esercizio 8, usando un ciclo logga i numeri da 1 a 100  mettendo un asterisco vicino ai multipli di 7
+//  for (let i = 1; i <= 100; i++) {
+//      if (i % 7 === 0) {
+//          console.log(i + "*");
+//      } else {
+//          console.log(i);
+//      }
+//  }
+
+
 //Esercizio 9, usando un ciclo logga i numeri da 1 a 100 mettendo un  asterisco vicino ai quadrati pefetti
-//Esercizio 10, chiedi all'utetnte un numero  (Positivo) di partenza e poi logga tutti i numeri dal
+// for (let i = 1; i <= 100; i++) {
+//     if (Number.isInteger(Math.sqrt(i))) {
+//         console.log(i + "*");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+
+// 10) chiedi all'utente un numero (positivo) di partenza e poi logga tutti i numeri dal numero dato fino a 0
+// const numero = prompt("Inserisci un numero positivo:");
+// if (numero > 0) {
+//     for (let i = numero; i >= 0; i--) {
+//         console.log(i);
+//     }
+// } else {
+//     console.log("Inserisci un numero positivo!");
+// }
+
+
+// 11) chiedi all'utente due numeri e logga tutti i numeri pari compresi tra i numeri forniti dall'utente
+const numero1 = parseInt(prompt("Inserisci il primo numero:"));
+const numero2 = parseInt(prompt("Inserisci il secondo numero:"));
+
+
+
+// 12) chiedi all'utente quanti anni ha e digli quante ore ha gia' vissuto
